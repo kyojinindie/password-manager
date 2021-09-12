40ab4deadb2807a53be978ac65976b4f6127cc16
kyojinindie <kyojinindie@gmail.com>
2021-09-12 10:09:56 -0500

    Initial commit

---
+++ password-manager-service/.eslintrc.js
@@ -0,0 +1,16 @@
+ module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    jasmine: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+  }
+}
